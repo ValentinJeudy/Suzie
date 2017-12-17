c3ee71696d590613d68804bdfba0da1ee89f08f0
ValentinJeudy <valentin.jeudy@gmail.com>
2017-12-17 16:14:42 +0100

    making it 'work'

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>allez pd !!!!!!!</h2>
+    <p>yolo</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      yolo: 'My header here'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <header></header>
+    <h1>my Home</h1>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+  name: 'Home',
+  components: {
+    'Header': Header
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Homepage'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+  color: red;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
