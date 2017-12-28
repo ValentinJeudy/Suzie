58a9ab6bc0a8b31dbede4fbac03ad42da0ede9af
ValentinJeudy <valentin.jeudy@gmail.com>
2017-12-28 22:01:40 +0100

    Making router match with components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Roster from '@/components/Roster'
+import Artist from '@/components/Artist'
+import EventsList from '@/components/EventsList'
+import Contact from '@/components/Contact'
 
 Vue.use(Router)
 
@@ -10,6 +14,26 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/roster',
+      name: 'Roster',
+      component: Roster
+    },
+    {
+      path: '/artist',
+      name: 'Artist',
+      component: Artist
+    },
+    {
+      path: '/events',
+      name: 'EventsList',
+      component: EventsList
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
     }
   ]
 })
