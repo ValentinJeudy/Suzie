1e5958c9f1795c4919d2bb1a48b7ca841bb452e8
val <valentin.jeudy@gmail.com>
2018-09-30 16:41:06 +0200

    moving from essential vue plugin linting to recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['plugin:vue/essential', 'standard'],
+  extends: ['plugin:vue/recommended', 'standard'],
   rules: {
     'vue/html-no-self-closing': 0,
     'arrow-parens': 1
